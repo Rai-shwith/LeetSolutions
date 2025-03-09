fb1223d5de21795caa1c31581eb7cc753c7d167c
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-03-09 09:13:06 +0530

    Automated upload: User's LeetCode solutions added in bulk

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,1 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        table = {}
-        for i in range(len(nums)):
-            if target-nums[i] in table:
-                return [table[target-nums[i]],i]
-            else:
-                table[nums[i]] = i
-        
+Testing 
